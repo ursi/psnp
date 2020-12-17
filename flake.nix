8ef9bfa0a19eba3f2508120b2bcf61d33e9f3560
Mason Mackaman <masondeanm@aol.com>
2020-12-16 19:13:24 -0500

    Add meta attribute

--- flake.nix
+++ flake.nix
@@ -3,10 +3,24 @@
     utils.defaultSystems
       (pkgs: with pkgs;
         {
-          defaultPackage = import ./psnp.nix {
+          defaultPackage = (import ./psnp.nix {
             inherit pkgs;
             runtimeDeps = [ dhall-json git ];
-          };
+          }).overrideAttrs
+            (_: {
+                meta = {
+                  description = "PureScript Nix Packager - generate nix packages from PureScript projects";
+                  homepage = "https://github.com/ursi/psnp";
+                  maintainers = [
+                    {
+                      name = "Mason Mackaman";
+                      email = "masondeanm@aol.com";
+                      github = "ursi";
+                      githubId = "17836748";
+                    }
+                  ];
+                };
+            });
 
           devShell = mkShell {
             buildInputs = [
