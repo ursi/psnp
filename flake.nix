b094e907a5ef7dd3809157530cb598dce20e63fa
Mason Mackaman <masondeanm@aol.com>
2021-04-01 19:48:58 -0400

    Reformat

--- flake.nix
+++ flake.nix
@@ -1,63 +1,64 @@
-rec {
-  description = "PureScript Nix Packager - generate nix packages from PureScript projects";
+rec
+{ description = "PureScript Nix Packager - generate nix packages from PureScript projects";
   inputs.dhall-to-nix.url = "github:ursi/dhall-to-nix";
 
-  outputs = { self, nixpkgs, utils, dhall-to-nix }:
+  outputs = { nixpkgs, utils, dhall-to-nix, ... }:
     utils.defaultSystems
       ({ pkgs, system }: with pkgs;
-        let
-          inherit (dhall-to-nix system ./name-version.dhall) name version;
-        in
-          {
-            defaultPackage =
-              let
-                base = import (./. + "/${name}.nix") {
-                  inherit (nixpkgs) lib;
-                  inherit pkgs;
-                };
-              in
-                base.overrideAttrs
-                  (old:
-                    {
-                      buildInputs = [ dhall-json git ] ++ old.buildInputs;
+         let
+           inherit (dhall-to-nix system ./name-version.dhall) name version;
+         in
+           { defaultPackage =
+               let
+                 base =
+                   import (./. + "/${name}.nix")
+                     { inherit (nixpkgs) lib;
+                       inherit pkgs;
+                     };
+               in
+                 base.overrideAttrs
+                   (old:
+                      { buildInputs = [ dhall-json git ] ++ old.buildInputs;
 
-                      installPhase = base.mkInstallPhase "${name} ${version}";
+                        installPhase = base.mkInstallPhase "${name} ${version}";
 
-                      meta = {
-                        inherit description;
-                        homepage = "https://github.com/ursi/${name}";
+                        meta =
+                          { inherit description;
+                            homepage = "https://github.com/ursi/${name}";
 
-                        maintainers = [
-                          {
-                            name = "Mason Mackaman";
-                            email = "masondeanm@aol.com";
-                            github = "ursi";
-                            githubId = "17836748";
-                          }
-                        ];
-                      };
-                    });
+                            maintainers =
+                              [ { name = "Mason Mackaman";
+                                  email = "masondeanm@aol.com";
+                                  github = "ursi";
+                                  githubId = "17836748";
+                                }
+                              ];
+                          };
+                      }
+                   );
 
-            devShell = mkShell {
-              buildInputs = [
-                dhall
-                dhall-json
-                purescript
-                nodejs
-                spago
-              ];
+             devShell =
+               mkShell
+                 { buildInputs =
+                     [ dhall
+                       dhall-json
+                       purescript
+                       nodejs
+                       spago
+                     ];
 
-              shellHook = ''
-                echo 'To run the current PureScript code as ${name}, just use the "${name}" command provided by the "shellHook".'
+                   shellHook =
+                     ''
+                     echo 'To run the current PureScript code as ${name}, just use the "${name}" command provided by the "shellHook".'
 
-                ${name} () {
-                  spago bundle-app
-                  node . ${name} ${version}
-                  rm index.js
-                }
-              '';
-            };
-          }
+                     ${name}() {
+                       spago bundle-app
+                       node . ${name} ${version}
+                       rm index.js
+                     }
+                     '';
+                 };
+           }
       )
       nixpkgs;
 }
